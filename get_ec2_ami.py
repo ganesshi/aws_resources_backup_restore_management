8224cc9e3643ba5dd0cd83f3956665505480467a
ganesshi <ganesshi8087@gmail.com>
2021-02-22 15:06:37 +0530

    Update get_ec2_ami.py

--- get_ec2_ami.py
+++ get_ec2_ami.py
@@ -4,6 +4,7 @@ def lambda_handler(event, context):
     account_id = boto3.client('sts').get_caller_identity().get('Account')
     reg = event.Region
     ec2 = boto3.client('ec2', region_name=reg)
+    #comment 2
     resp = ec2.describe_images(Filters=[{'Name': 'owner-id', 'Values': [account_id]}])
     image_list = []
     for image in resp['Images']:
